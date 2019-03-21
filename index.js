ecfb39c3ce3ada39ce14f78b5c526982b3a4fecf
Rob Versluis <versluis.rob@hotmail.com>
2019-03-21 22:33:54 +1100

    fixed heroku port issue

--- index.js
+++ index.js
@@ -213,6 +213,6 @@ io.on('connection', (socket) => {
     })
 })
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log('Stratego server listening on PORT: 3000')
 })
